722e5be49afa2d533ad830b62b1ca5cdc5ab2c23
Sam Minot <sminot@fredhutch.org>
2023-01-09 13:47:21 -0800

    Remove code which matches the parent

--- src/widgets/streamlit/widget.py
+++ src/widgets/streamlit/widget.py
@@ -24,46 +24,6 @@ class StreamlitWidget(Widget):
     ]
     extra_imports:List[str] = []
 
-    def __init__(self, data=dict()) -> Widget:
-        """
-        Set up the StreamlitWidget object.
-        Optionally provide input data which will override the default values for each
-        of the resources defined by this widget.
-        """
-
-        if not isinstance(data, dict):
-            raise WidgetInitializationException(f"data must be a dict, not a {type(data)}")
-
-        # Attach the data provided at initialization
-        self.data = data
-
-        # Iterate over each resource defined in the widget
-        for resource in self.resources:
-
-            # Make sure that the resource is a recognized type
-            if not isinstance(resource, Resource):
-                raise WidgetConfigurationException("All resources must be a derivative of StreamlitResource")
-
-            # If any data was provided at the time of initialization, use that to
-            # override the default value defined for the resource
-            resource._setup_default(data.get(resource.id))
-
-            # Populate the initial state of the `data` object
-            self.data[resource.id] = resource.default
-
-    def run(self) -> None:
-        """
-        Primary entrypoint used to launch the widget.
-
-        1. Run the user_input() method for all resources defined in the widget;
-        2. Invoke the viz() function;
-        3. Add buttons extending functionality of the widget;
-        """
-        
-        self.inputs()
-        self.viz()
-        self.extra_functions()
-
     def run_cli(self, args:List[str]=[], flag_options:Dict[str,Any]={}, title="Widget") -> None:
         """
         Run the widget from the command line.
@@ -99,17 +59,6 @@ class StreamlitWidget(Widget):
 
         # Return the file object
         return fp
-
-    def inputs(self) -> None:
-        """Read in data from all of the resources defined in the widget."""
-        
-        # Iterate over each of the resources defined for this widget
-        for resource in self.resources:
-
-            # Add the interactive input element, if any has been defined
-            # We pass in the data object for the widget so that the value
-            # can be updated when the user input changes
-            resource.user_input(self.data)
 
     def viz(self) -> None:
         """The viz() method should be overridden by any widget based on this class."""
