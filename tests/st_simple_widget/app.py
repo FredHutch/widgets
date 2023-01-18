a75ea50aa901ff31c5a1f9feba6dc5985ff5b686
Sam Minot <sminot@fredhutch.org>
2023-01-18 09:55:34 -0800

    Fix PyPI package name

--- tests/st_simple_widget/app.py
+++ tests/st_simple_widget/app.py
@@ -35,7 +35,7 @@ class SimpleWidget(StreamlitWidget):
     # Specify any packages which should be installed prior to loading
 
     requirements = [
-        "widgets",
+        "widgets-lib",
         "pandas",
         "plotly"
     ]
