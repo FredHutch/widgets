636943f230d66ce2e255865d3378435f186a0bc7
Sam Minot <sminot@fredhutch.org>
2023-01-26 12:00:38 -0800

    Add the update flag to the StWidget's set function

--- src/widgets/streamlit/widget.py
+++ src/widgets/streamlit/widget.py
@@ -171,3 +171,15 @@ class StreamlitWidget(Widget):
         """Return the UI identifier for a resource."""
 
         return self._get_resource(resource_id).key()
+
+    def set(self, resource_id, attr, val, update=True):
+        """Set the value of an attribute of a resource."""
+
+        # Call the set function of the resource
+        self._get_resource(resource_id).set(attr, val, update=update)
+
+    def set_value(self, resource_id, val, update=True):
+        """Set the 'value' attribute of a resource."""
+
+        # Call the set function of the resource
+        self.set(resource_id, "value", val, update=update)
