b33f8d3b473e967975e0ebd166579a553b8c05e8
Sam Minot <sminot@fredhutch.org>
2023-01-31 14:51:44 -0800

    Base Widget on ResourceList

--- src/widgets/base/widget.py
+++ src/widgets/base/widget.py
@@ -1,44 +1,22 @@
 from inspect import getmro, getsource, isfunction
+import logging
 from pathlib import Path
-from typing import Any, Dict, List, Union
-from widgets.base.resource import Resource
+from typing import Union
+from uuid import uuid4
 from widgets.base.exceptions import CLIExecutionException
-from widgets.base.exceptions import WidgetConfigurationException
 from widgets.base.exceptions import WidgetFunctionException
 from widgets.base.helpers import render_template
+from widgets.base.resource_list import ResourceList
 
 
-class Widget:
+class Widget(ResourceList):
     """
     Base class used for building interactive widgets.
     The list of resources will be mapped by .id to the
-    resource_dict at initialization.
+    _resource_dict at initialization.
     """
 
-    resources: List[Resource] = list()
-    resource_dict: Dict[str, Resource] = dict()
-
-    def __init__(self):
-        """
-        Set up the Widget object
-        """
-
-        # The resource_dict must be empty at initialization
-        self.resource_dict = dict()
-
-        # Iterate over each resource defined in the widget
-        for resource in self.resources:
-
-            # Make sure that the resource is a recognized type
-            if not isinstance(resource, Resource):
-                msg = "All resources must be a derivative of Resource"
-                raise WidgetConfigurationException(msg)
-
-            # Make sure that the id attribute is not repeated
-            if resource.id in self.resource_dict:
-                msg = f"Resource ids must be unique (repeated: {resource.id})"
-                raise WidgetConfigurationException(msg)
-            self.resource_dict[resource.id] = resource
+    resource_container = None
 
     def run(self) -> None:
         """
@@ -46,12 +24,12 @@ class Widget:
 
         1. Run the setup_ui() method for all resources defined in the widget;
         2. Invoke the viz() function;
-        3. Add buttons extending functionality of the widget;
         """
+        self.rand = uuid4()
+        logging.info(f"Widget - run ({self.rand})")
 
         self.inputs()
         self.viz()
-        self.extra_functions()
 
     def run_cli(self) -> None:
         """
@@ -63,53 +41,14 @@ class Widget:
     def inputs(self) -> None:
         """Read in data from all of the resources defined in the widget."""
 
-        # Iterate over each of the resources defined for this widget
-        for resource in self.resources:
-
-            # Add the interactive input element, if any has been defined
-            resource.setup_ui()
-
-    def get(self, resource_id: str, attr: str) -> Any:
-        """Get the value of an attribute of a resource."""
-
-        # Return the value provided by the get method of the resource
-        return self._get_resource(resource_id).get(attr)
-
-    def get_value(self, resource_id: str) -> Any:
-        """Get the 'value' attribute of a resource."""
-
-        return self._get_resource(resource_id).get_value()
-
-    def set(self, resource_id, attr, val):
-        """Set the value of an attribute of a resource."""
-
-        # Call the set function of the resource
-        self._get_resource(resource_id).set(attr, val)
-
-    def set_value(self, resource_id, val):
-        """Set the 'value' attribute of a resource."""
-
-        # Call the set function of the resource
-        self.set(resource_id, "value", val)
-
-    def _get_resource(self, resource_id) -> Resource:
-        """Return the resource with a corresponding id."""
-
-        # Get the resource
-        r = self.resource_dict.get(resource_id)
-
-        if r is None:
-            raise WidgetFunctionException(f"No resource exists: {resource_id}")
-        return r
+        # This method will recursively run setup_ui for each Resource
+        logging.info(f"Widget - inputs ({self.rand})")
+        self.setup_ui(self.resource_container)
 
     def viz(self) -> None:
         """
         The viz() method should be overridden by any widget based on this.
         """
-        pass
-
-    def extra_functions(self) -> None:
-        """Add generalized functionality to the widget."""
         pass
 
     def to_html(self, fp: Union[Path, None] = None) -> Union[None, str]:
@@ -177,7 +116,6 @@ class Widget:
             "source.py.j2",
             name=self._name(),
             parent_name=self._parent_name(),
-            resources=self._source_resources(),
             attributes=self._source_attributes(),
             functions=self._source_functions()
         )
@@ -217,30 +155,9 @@ class Widget:
             if filter_functions == isfunction(val):
                 yield kw, val
 
-    def _source_resources(self, indent=4) -> str:
-        """
-        Return a string which captures the source code needed to initialize
-        the resources attached to this object.
-        """
-        spacer = "".join([" " for _ in range(indent)])
-
-        # Format the source code for each of the resources in this widget
-        resources_str = []
-        for r in self.resources:
-
-            # Format the source code and append it to the lsit
-            resources_str.append(r.source())
-
-        # Join all of those resource strings into a list
-        line_spacer = f",\n{spacer}{spacer}"
-        resources_str = f"    resources = [\n{spacer}{spacer}{line_spacer.join(resources_str)}\n{spacer}]" # noqa
-
-        return resources_str
-
-    def _source_attributes(self, omit=["resources", "resource_dict"]) -> str:
+    def _source_attributes(self) -> str:
         """
         Return a text block which captures the attributes of this class.
-        Any attributes in the omit list will be omitted.
         """
 
         attributes = []
@@ -248,11 +165,8 @@ class Widget:
         # Iterate over the attributes of this class
         for kw, attrib in self._class_items(filter_functions=False):
 
-            # If the attribute is not in the omit list
-            if kw not in omit:
-
-                # Add it to the list
-                attributes.append(f"    {kw} = {self.source_val(attrib)}")
+            # Add it to the list
+            attributes.append(f"    {kw} = {self._source_val(attrib)}")
 
         return "\n\n".join(attributes)
 
@@ -266,14 +180,3 @@ class Widget:
             getsource(func)
             for _, func in self._class_items(filter_functions=True)
         ])
-
-    def source_val(self, val):
-        """
-        Return a string representation of an attribute value
-        which can be used in source code initializing this widget.
-        """
-
-        if isinstance(val, str):
-            return f'"{val}"'
-        else:
-            return val
