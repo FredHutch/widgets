782837e996d42d4f621792f859c58a26bad8046c
Sam Minot <sminot@fredhutch.org>
2023-02-22 12:20:24 -0800

    Add MultiSelect resource

--- src/widgets/streamlit/__init__.py
+++ src/widgets/streamlit/__init__.py
@@ -7,6 +7,7 @@ from widgets.streamlit.resource.value import StCheckbox # noqa
 from widgets.streamlit.resource.value import StFloat # noqa
 from widgets.streamlit.resource.value import StInteger # noqa
 from widgets.streamlit.resource.value import StSelectString # noqa
+from widgets.streamlit.resource.value import StMultiSelect # noqa
 from widgets.streamlit.resource.value import StString # noqa
 from widgets.streamlit.resource.value import StSlider # noqa
 from widgets.streamlit.resource.markdown import StMarkdown # noqa
